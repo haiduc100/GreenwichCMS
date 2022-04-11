6bb2c389929dffeecc9c45b78d96a0c12c66d0cc
haiduc100 <ducnvgch190188@fpt.edu.vn>
2022-04-11 08:05:12 +0700

    add email migration

--- Migrations/20220411010219_email.Designer.cs
+++ Migrations/20220411010219_email.Designer.cs
@@ -4,14 +4,16 @@ using GreenwichCMS.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GreenwichCMS.Migrations
 {
     [DbContext(typeof(GreenwichContext))]
-    partial class GreenwichContextModelSnapshot : ModelSnapshot
+    [Migration("20220411010219_email")]
+    partial class email
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Users.cs
+++ Models/Users.cs
@@ -13,7 +13,7 @@ namespace GreenwichCMS.Models
         [Key]
         public Guid UserId { get; set; }
         public string FirstName { get; set; }
-        public string Email { get;set; }
+        public string Email { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string UserName { get; set; }
